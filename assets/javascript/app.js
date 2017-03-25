0e4c0c2a9fbce21fe8fd96f44be632a9ec833720
Steven Jirjis <sjirjis@tessituranetwork.com>
2017-03-24 19:38:02 -0700

    initial push

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	
+	$("#orgCode").keypress(function(e){
+		if(e.keyCode === 13){
+			$("#orgCodeBtn").click();
+		} else {
+			return;
+		}
+	});
+
+	$("#orgCodeBtn").on("click", function(){
+		var orgCode = $("#orgCode").val().trim(),
+			region = $("#regionSelect option:selected").val().trim(),
+			ssURL = "http://production-"+region+"-proxy.tnew.info/"+orgCode+"/tessitura.asmx/WebSeatServerConnectionCount";
+
+		apiCall(ssURL);
+	});
+
+	function apiCall(url){
+		$.ajax({URL: url, method: "GET"})
+			.done(function(response){
+				console.log(response);
+			})
+	}
+});
